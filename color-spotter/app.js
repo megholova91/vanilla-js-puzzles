84e09fab0eb559cc5f38fddbbc51ef5074b242fb
Megholova Mukherjee <megholova@gmail.com>
2024-05-12 16:38:54 +0530

    Updated color spotter

--- color-spotter/app.js
+++ color-spotter/app.js
@@ -16,64 +16,74 @@
  *
  **/
 
-const gridContainer = document.getElementById('grid-container');
-const scoreContainer = document.getElementById('score');
-var n = 4;
-var score = 0;
-scoreContainer.innerHTML = score;
+(() => {
+  let n = 3,
+    score = 0;
 
-const renderGrid = () => {
-    clearGrid();
-    const color = generateRandomColor();
-    const randomRow = Math.floor(Math.random() * n);
-    const randomCol = Math.floor(Math.random() * n);
-    for (let row = 0; row < n; row++) {
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('grid-row');
-        for (let col = 0; col < n; col++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.style.background = color;
-            const uniqueCell = row === randomRow && col === randomCol;
-            if (uniqueCell) {
-                cell.style.opacity = '0.8';
-            }
-            cell.onclick = function () {
-                if (uniqueCell) {
-                    n++;
-                    score++;
-                    renderGridWithScore();
-                } else {
-                    score = 0;
-                    n = 4;
-                    gridContainer.classList.add('game-over');
-                    setTimeout(() => {
-                        renderGridWithScore();
-                    }, 900);
-                }
+  let ansRow = Math.floor(Math.random() * n);
+  let ansCol = Math.floor(Math.random() * n);
 
-            }
-            gridRow.appendChild(cell);
+  const gridContainer = document.getElementById("grid-container");
+  const scoreContainer = document.getElementById("score");
+
+  scoreContainer.innerHTML = score;
+
+  function initializeGrid(color) {
+    //call reset grid
+    resetGrid();
+    for (let i = 0; i < n; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      for (let j = 0; j < n; j++) {
+        const col = document.createElement("div");
+        col.classList.add("col");
+        col.style.backgroundColor = color;
+        if (i === ansRow && j === ansCol) {
+          col.style.opacity = "0.8";
         }
-        gridContainer.appendChild(gridRow);
+        col.setAttribute("data-item", `${i}-${j}`);
+        row.appendChild(col);
+      }
+      gridContainer.appendChild(row);
     }
+  }
 
-}
+  function resetGrid() {
+    gridContainer.innerText = "";
+  }
 
-const clearGrid = () => {
-    while (gridContainer.hasChildNodes()) {
-        gridContainer.lastChild.remove();
+  function resetGame(newScore, newDim) {
+    score = newScore;
+    n = newDim;
+    scoreContainer.innerText = score;
+    ansRow = Math.floor(Math.random() * n);
+    ansCol = Math.floor(Math.random() * n);
+    gridContainer.classList.remove("game-over");
+    initializeGrid(generateRandomColor());
+  }
+
+  const generateRandomColor = () => {
+    return "#" + (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+  };
+
+  const shakeGrid = () => {
+    gridContainer.classList.add("game-over");
+  };
+
+  gridContainer.addEventListener("click", (evt) => {
+    const { item } = evt.target.dataset;
+    const [row, col] = item.split("-");
+    if (ansRow === Number(row) && ansCol === Number(col)) {
+      score++;
+      n++;
+      resetGame(score, n);
+    } else {
+      shakeGrid();
+      setTimeout(function () {
+        resetGame(0, 3);
+      }, 800);
     }
-    gridContainer.classList.remove('game-over');
-}
+  });
 
-const renderGridWithScore = () => {
-    renderGrid();
-    scoreContainer.innerHTML = score;
-}
-
-const generateRandomColor = () => {
-    return '#' + (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
-}
-
-renderGrid();
+  initializeGrid(generateRandomColor());
+})();
