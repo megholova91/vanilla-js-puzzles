540522efdac00f9718a194be0383aa0c7a9ba99e
Megholova Mukherjee <megholova@gmail.com>
2024-05-13 23:41:01 +0530

    Updated memory game logic + made mobile responsive

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,92 +1,98 @@
-let currentScore = 0, highScore = parseInt(localStorage.getItem('highScore'), 10) || 0;
-const numberOfBlocks = 5;
-let round = 1, currentIndex = 0, givenSequence, roundInProgress = false;
+(() => {
+  const numberOfBlocks = 5;
 
-const currentScoreContainer = document.getElementById('current-score');
-const highScoreContainer = document.getElementById('high-score');
+  let currentScore = 0,
+    highScore = parseInt(localStorage.getItem("highScore"), 10) || 0;
+  let round = 1,
+    currentIndex = 0,
+    givenSequence = [],
+    roundInProgress = false;
 
-currentScoreContainer.innerHTML = currentScore;
-highScoreContainer.innerHTML = highScore;
+  const currentScoreContainer = document.getElementById("current-score");
+  const highScoreContainer = document.getElementById("high-score");
+  const blocksContainer = document.getElementById("blocks-container");
+  const startButton = document.getElementById("start-button");
 
-//create the blocks
-const blocksContainer = document.getElementById('blocks-container');
+  currentScoreContainer.innerText = currentScore;
+  highScoreContainer.innerText = highScore;
 
-for (let count = 0; count < numberOfBlocks; count++) {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    block.id = `block-${count}`;
-    block.onclick = function () {
-        if (!roundInProgress) {
-            gameScoreCheck(count);
-            block.classList.add('click');
-            setTimeout(() => {
-                block.classList.remove('click');
-            }, 200);
-        }
+  const createBlocks = () => {
+    for (let count = 0; count < numberOfBlocks; count++) {
+      const block = document.createElement("div");
+      block.classList.add("block");
+      block.dataset.count = count;
+      blocksContainer.appendChild(block);
     }
-    blocksContainer.appendChild(block);
-}
+  };
 
-function gameScoreCheck(blockId) {
-    if (givenSequence[currentIndex] == blockId) {
-        currentIndex++;
-        console.log('correct');
-        //when the round is over
-        if (currentIndex == round) {
-            currentScore++;
-            updateHighScore();
-            ++round;
-            playCurrentRound();
-        }
-    } else {
-        blocksContainer.classList.add('game-over');
-        console.log('game over');
-        currentScore = 0;
-        startButton.disabled = false;
-        round = 1;
-    }
-    currentScoreContainer.innerHTML = currentScore;
-}
-
-function updateHighScore() {
-    if (currentScore > highScore) {
-        highScore = currentScore;
-        highScoreContainer.innerHTML = highScore;
-        localStorage.setItem('highScore', highScore.toString());
-    }
-}
-
-//start button click function
-const startButton = document.getElementById('start-button');
-
-startButton.onclick = function () {
+  //start button click function
+  startButton.onclick = function () {
     startButton.disabled = true;
-    blocksContainer.classList.remove('game-over');
+    blocksContainer.classList.remove("game-over");
     playCurrentRound();
-}
+  };
 
-function playCurrentRound() {
-    autoBlink();
-}
-
-//to blink random blocks 'round' number of times
-function autoBlink() {
+  //to blink random blocks 'round' number of times
+  function playCurrentRound() {
     roundInProgress = true;
     givenSequence = [];
     let count = 0;
     const intervalId = setInterval(() => {
-        const randomBlockId = Math.floor(Math.random() * numberOfBlocks);
-        givenSequence.push(randomBlockId);
-        const block = document.getElementById(`block-${randomBlockId}`);
-        block.classList.add('blink');
-        setTimeout(() => {
-            block.classList.remove('blink');
-        }, 500);
-        if (++count == round) {
-            window.clearInterval(intervalId);
-            roundInProgress = false;
-            userSequence = [];
-            currentIndex = 0;
-        }
+      const randomBlockNumber = Math.floor(Math.random() * numberOfBlocks);
+      givenSequence.push(randomBlockNumber);
+      const block = document.querySelector(
+        `[data-count="${randomBlockNumber}"]`
+      );
+      block.classList.add("blink");
+      setTimeout(() => {
+        block.classList.remove("blink");
+      }, 500);
+      if (++count == round) {
+        window.clearInterval(intervalId);
+        roundInProgress = false;
+        userSequence = [];
+        currentIndex = 0;
+      }
     }, 1000);
-}
+  }
+
+  blocksContainer.addEventListener("click", (event) => {
+    const { count } = event.target.dataset;
+    if (count && !roundInProgress) {
+      gameScoreCheck(count);
+      event.target.classList.add("click");
+      setTimeout(() => {
+        event.target.classList.remove("click");
+      }, 200);
+    }
+  });
+
+  function gameScoreCheck(blockNumber) {
+    if (givenSequence[currentIndex] == blockNumber) {
+      currentIndex++;
+      //when the round is over
+      if (currentIndex == round) {
+        currentScore++;
+        updateHighScore();
+        ++round;
+        playCurrentRound();
+      }
+    } else {
+      blocksContainer.classList.add("game-over");
+      currentScore = 0;
+      startButton.disabled = false;
+      round = 1;
+    }
+    currentScoreContainer.innerText = currentScore;
+  }
+
+  function updateHighScore() {
+    if (currentScore > highScore) {
+      highScore = currentScore;
+      highScoreContainer.innerText = highScore;
+      localStorage.setItem("highScore", highScore.toString());
+    }
+  }
+
+  createBlocks();
+})();
