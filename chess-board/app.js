ebd521f4b685f572d30654406cbe537f25e749bf
Megholova Mukherjee <megholova@gmail.com>
2024-05-14 00:11:18 +0530

    Fixed chessboard code + mobile responsive

--- chess-board/app.js
+++ chess-board/app.js
@@ -1,33 +1,41 @@
-const chessboardContainer = document.getElementById('chessboard-container');
-const matrixDim = 8;
+(() => {
+  const chessboardContainer = document.getElementById("chessboard-container");
+  const matrixDim = 8;
 
-for (let row = 0; row < matrixDim; row++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.classList.add('rowContainer');
-    for (let col = 0; col < matrixDim; col++) {
-        const square = document.createElement('div');
+  const createGrid = () => {
+    for (let row = 0; row < matrixDim; row++) {
+      const rowContainer = document.createElement("div");
+      rowContainer.classList.add("rowContainer");
+      for (let col = 0; col < matrixDim; col++) {
+        const square = document.createElement("div");
         if (row % 2 == 0) {
-            square.classList.add('square');
+          square.classList.add("square");
         } else {
-            square.classList.add('evenSquare');
+          square.classList.add("evenSquare");
         }
-        square.id = `${row}-${col}`;
+        square.dataset.cell = `${row}-${col}`;
         rowContainer.appendChild(square);
-
-        square.onclick = function () {
-            for (let i = 0; i < matrixDim; i++) {
-                for (let j = 0; j < matrixDim; j++) {
-                    const diagonalElement = document.getElementById(`${i}-${j}`);
-                    if (diagonalElement) {
-                        if (Math.abs(row - i) == Math.abs(col - j)) {
-                            diagonalElement.classList.add('highlighted-cell');
-                        } else {
-                            diagonalElement.classList.remove('highlighted-cell');
-                        }
-                    }
-                }
-            }
-        }
+      }
+      chessboardContainer.appendChild(rowContainer);
     }
-    chessboardContainer.appendChild(rowContainer);
-}
+  };
+
+  chessboardContainer.onclick = (event) => {
+    const { cell } = event.target.dataset;
+    if (cell) {
+      const [row, col] = cell.split("-");
+      for (let i = 0; i < matrixDim; i++) {
+        for (let j = 0; j < matrixDim; j++) {
+          const elem = document.querySelector(`[data-cell="${i}-${j}"`);
+          if (Math.abs(row - i) == Math.abs(col - j)) {
+            elem.classList.add("highlighted-cell");
+          } else {
+            elem.classList.remove("highlighted-cell");
+          }
+        }
+      }
+    }
+  };
+
+  createGrid();
+})();
